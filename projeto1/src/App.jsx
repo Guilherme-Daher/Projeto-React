43ee0146ba056f6092ff27e391dc27815c6e67ac
Guilherme-Daher <RM98611@fiap.com.br>
2023-09-19 00:44:59 -0300

    alteração nos imports

--- projeto1/src/App.jsx
+++ projeto1/src/App.jsx
@@ -1,77 +1,62 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Todo from './components/todo';
+import Todo from "./componentes/Todo";
 
-import TodoForm from './components/TodoForm';
+import TodoForm from "./componentes/TodoForm";
 
-import Search from './components/Search';
+import Search from "./componentes/Search";
 
-import Filter from './components/Filter';
- 
+import Filter from "./componentes/Filter";
+
 import "./App.css";
 
 
-
-
-
-function App() {
+const App = () => {
   const [todos, setTodos] = useState([
     {
-      id:1,
-      text: "Criar funcionalidade x no sistema",
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
       category: "Trabalho",
       isCompleted: false,
     },
+    { id: 2,text: "Ir para a academia", category: "Pessoal", isCompleted: false },
     {
-      id:2,
-      text: "Ir pra academia",
-      category: "Pessoal",
-      isCompleted: false,
-    },
-    {
-      id:3,
+      id: 3,
       text: "Estudar React",
       category: "Estudos",
       isCompleted: false,
-    }
-  ])
+    },
+  ]);
+
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const [search, setSearch] = useState("");
 
-  const {filter, setFilter} = useState("All");
-  const {sort, setSort} = useState("Asc");
-  
-  function addTodo(text, category) {
-
-    const newTodos = [...todos, {
-      id: Math.floor(Math.random() * 10000),
-      text,
-      category,
-      isCompleted: false,
-    },
+  const addTodo = (text, category) => {
+    const newTodos = [...todos, 
+      { id: Math.floor(Math.random() * 1000), text, category, isCompleted: false }
     ];
     setTodos(newTodos);
-  }
+  };
 
   const removeTodo = (id) => {
-    const newTodos = [... todos]
-    const filterdeTodos = newTodos.filter(todo => 
-      todo.id !== id ? todo : null 
-    );
-    setTodos(filterdeTodos);   
-  }
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) => todo.id !== id ?  todo : null)
+    setTodos(filteredTodos);
+  };
 
-  const completeTodo = (id) =>{
-    const newTodos = [... todos]
-    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted: todo)
-    setTodos(newTodos)
-  }
-  
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) => todo.id === id ?  todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos);
+  };
+
   return (
     <div className="app">
-      <h1>Minhas tarefas</h1>
+      <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter}   />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -80,22 +65,28 @@ function App() {
               : filter === "Completed"
               ? todo.isCompleted
               : !todo.isCompleted
-        )
-        .filter((todo) => 
-          todo.text.toLowerCase().includes(search.toLowerCase())
-        )
-        .map((todo) => (
-          <Todo 
-            key={todo.id}
-            todo={todo}
-            removeTodo={removeTodo}
-            completeTodo={completeTodo}/> 
-        ))} 
+          )
+          .filter((todo) =>
+            todo.text.toLowerCase().includes(search.toLowerCase())
+          )
+          .sort((a, b) =>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+          )
+          .map((todo, index) => (
+            <Todo
+              key={index}
+              index={index}
+              todo={todo}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+            />
+          ))}
       </div>
-          <TodoForm addTodo={addTodo} />
+      <TodoForm addTodo={addTodo} />
     </div>
-    
-    );
-}
+  );
+};
 
 export default App;
